84993738ca47d2ef33eb36298c76e866df7fc845
Raul Casado Moreno <racasado@c3r2s6.42malaga.com>
2025-02-10 19:51:53 +0100

    yapping

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,17 +6,31 @@
 /*   By: racasado <racasado@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:33:10 by racasado          #+#    #+#             */
-/*   Updated: 2025/02/10 18:09:50 by racasado         ###   ########.fr       */
+/*   Updated: 2025/02/10 19:50:56 by racasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-typedef struct s_minishell
+typedef enum e_token_type
 {
-	
-} t_minishell;
+	TOKEN_WORD, // normal word
+	TOKEN_PIPE, // |
+	TOKEN_REDIR_IN, // <
+	TOKEN_REDIR_OUT, // >
+	TOKEN_REDIR_APPEND, // >>
+	TOKEN_HEREDOC, // <<
+	// we could add TOKEN_ENV_VAR OR TOKEN_FLAG but right now its in TOKEN_WORD
+	TOKEN_COMMAND, // maybe join it with TOKEN_WORD because its difficult to differentiate
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
 
 #include <stdio.h>
 #include <stdlib.h>
