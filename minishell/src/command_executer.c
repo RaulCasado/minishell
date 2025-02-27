f4957a3db36b5a913be72abd5f3d5a3410cbd85c
RaulCasado <raulcasado969@gmail.com>
2025-02-27 21:34:29 +0100

    pipes functional (sometimes)

--- minishell/src/command_executer.c
+++ minishell/src/command_executer.c
@@ -1,49 +1,116 @@
 #include "minishell.h"
 
+static int ft_strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+static int is_builtin(char *cmd)
+{
+    if (!cmd)
+        return (0);
+    return (ft_strcmp(cmd, "echo") == 0 || ft_strcmp(cmd, "cd") == 0 ||
+            ft_strcmp(cmd, "pwd") == 0 || ft_strcmp(cmd, "export") == 0 ||
+            ft_strcmp(cmd, "unset") == 0 || ft_strcmp(cmd, "env") == 0 ||
+            ft_strcmp(cmd, "exit") == 0);
+}
+
+static void execute_command(t_minishell *minishell, t_command *cmd)
+{
+    if (ft_strcmp(cmd->args[0], "echo") == 0)
+        builtin_echo(minishell, cmd);
+    else if (ft_strcmp(cmd->args[0], "cd") == 0)
+        builtin_cd(minishell, cmd);
+    else if (ft_strcmp(cmd->args[0], "pwd") == 0)
+        builtin_pwd(minishell, cmd);
+    else if (ft_strcmp(cmd->args[0], "export") == 0)
+        builtin_export(minishell, cmd);
+    else if (ft_strcmp(cmd->args[0], "unset") == 0)
+        builtin_unset(minishell, cmd);
+    else if (ft_strcmp(cmd->args[0], "env") == 0)
+        builtin_env(minishell, cmd);
+    else if (ft_strcmp(cmd->args[0], "exit") == 0)
+        builtin_exit(minishell, cmd);
+    else
+        command_process(minishell, cmd);
+}
+// btw we should check the fd of every builtin funciton since the
+// redirections are handled in the execute_command function
+// instead of the builtin functions themselves for example
+// (no se como explicarme en ingles pero lo que teniamos de si el fd no es 1 xd y cosas asi)
+// other think we should check is echo hola | wc -c this command it counts the bytes of the string hola
+// rn it says its 80 bytes but it should be 5 bytes xd
 char command_executer(t_minishell *minishell)
 {
-    t_command   *cmd;
-    size_t      cmd_len;
-    int         saved_stdin;
-    int         saved_stdout;
+    t_command *cmd = minishell->commands;
+    int pipe_fd[2], prev_pipe_in = 0, num_commands = 0;
+    pid_t pid;
 
-    cmd = minishell->commands;
-    while (cmd)
+    // Count commands
+    t_command *tmp = cmd;
+    while (tmp)
     {
-        // Save original stdin/stdout
-        saved_stdin = dup(STDIN_FILENO);
-        saved_stdout = dup(STDOUT_FILENO);
-        
-        // Set up redirections
+        num_commands++;
+        tmp = tmp->next;
+    }
+
+    // Single builtin: execute in parent
+    if (num_commands == 1 && is_builtin(cmd->args[0]))
+    {
+        int saved_stdin = dup(STDIN_FILENO);
+        int saved_stdout = dup(STDOUT_FILENO);
         handle_redirections(cmd);
-        
-        cmd_len = ft_strlen(cmd->args[0]);
-        
-        // Execute the command based on its name
-        if (cmd_len == 4 && !ft_strncmp(cmd->args[0], "echo", 4))
-            builtin_echo(minishell, cmd);
-        else if (cmd_len == 2 && !ft_strncmp(cmd->args[0], "cd", 2))
-            builtin_cd(minishell, cmd);
-        else if (cmd_len == 3 && !ft_strncmp(cmd->args[0], "pwd", 3))
-            builtin_pwd(minishell, cmd);
-        else if (cmd_len == 6 && !ft_strncmp(cmd->args[0], "export", 6))
-            builtin_export(minishell, cmd);
-        else if (cmd_len == 5 && !ft_strncmp(cmd->args[0], "unset", 5))
-            builtin_unset(minishell, cmd);
-        else if (cmd_len == 3 && !ft_strncmp(cmd->args[0], "env", 3))
-            builtin_env(minishell, cmd);
-        else if (cmd_len == 4 && !ft_strncmp(cmd->args[0], "exit", 4))
-            builtin_exit(minishell, cmd);
-        else
-            command_process(minishell, cmd);
-        
-        // Restore original stdin/stdout
+        execute_command(minishell, cmd);
         dup2(saved_stdin, STDIN_FILENO);
         dup2(saved_stdout, STDOUT_FILENO);
         close(saved_stdin);
         close(saved_stdout);
-
-        cmd = cmd->next;
+        return (0);
     }
+
+    // Pipeline handling
+    while (cmd)
+    {
+        if (cmd->next && pipe(pipe_fd) == -1)
+            return (perror("minishell: pipe"), 1);
+
+        pid = fork();
+        if (pid == -1)
+            return (perror("minishell: fork"), 1);
+        else if (pid == 0) // Child
+        {
+            if (prev_pipe_in != 0)
+                dup2(prev_pipe_in, STDIN_FILENO);
+            if (cmd->next)
+                dup2(pipe_fd[1], STDOUT_FILENO);
+            if (prev_pipe_in != 0)
+                close(prev_pipe_in);
+            close(pipe_fd[0]);
+            if (cmd->next)
+                close(pipe_fd[1]);
+            handle_redirections(cmd);
+            execute_command(minishell, cmd);
+            exit(EXIT_SUCCESS);
+        }
+        else // Parent
+        {
+            if (prev_pipe_in != 0)
+                close(prev_pipe_in);
+            if (cmd->next)
+                prev_pipe_in = pipe_fd[0];
+            else
+                prev_pipe_in = 0;
+            close(pipe_fd[1]);
+            cmd = cmd->next;
+        }
+    }
+
+    // Wait for all children
+    while (waitpid(-1, NULL, 0) > 0);
+
     return (0);
 }
