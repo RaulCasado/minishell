85a6a0b4ba5a11a0dd81da670167335e09360f7b
RaulCasado <raulcasado969@gmail.com>
2025-03-05 18:45:34 +0100

    changed pwd to match the echo function

--- minishell/src/builtin_pwd.c
+++ minishell/src/builtin_pwd.c
@@ -1,19 +1,27 @@
 #include "minishell.h"
 
-char	builtin_pwd(t_minishell *minishell, t_command *command)
+char builtin_pwd(t_minishell *minishell, t_command *command)
 {
-	char	*arg;
+	char	*pwd;
 	char	fd;
 
-	if (command->args[1]) // There's only 1 arg in pwd
+	if (command->args[1]) // pwd no acepta argumentos
 		return (1);
-	arg = get_env(PWD, minishell->envp);
-	/* if (!arg)
-		error xd */
-	if (command->pipe_out == 0)
-		fd = 1;
-	/* else
-		fd = xd; */
-	write_str(arg, fd);
+
+	pwd = get_env("PWD", minishell->envp);
+	if (!pwd) // Si no encuentra PWD, usa `getcwd()`
+	{
+		char cwd[1024];
+		if (!getcwd(cwd, sizeof(cwd)))
+		{
+			perror("Minishell: pwd");
+			return (1);
+		}
+		pwd = cwd;
+	}
+
+	fd = STDOUT_FILENO; // Usamos el descriptor de salida estándar
+	write_str(pwd, fd);
+
 	return (0);
 }
