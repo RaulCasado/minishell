a2860893bb93d4630e65112788a9338caeb4947c
Daniel Roura <droura-s@c3r3s4.42malaga.com>
2025-03-26 13:19:24 +0100

    fix: fix problem with quotes

--- minishell/src/token_expander.c
+++ minishell/src/token_expander.c
@@ -6,7 +6,7 @@
 /*   By: droura-s <droura-s@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:33:35 by racasado          #+#    #+#             */
-/*   Updated: 2025/03/26 12:26:13 by droura-s         ###   ########.fr       */
+/*   Updated: 2025/03/26 13:10:08 by droura-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ static int	expand_loop(t_token *token, t_minishell *minishell,
 		return (0);
 	j = *i;
 	while (token->value[j + 1] && token->value[j + 1] != ' '
-		&& token->value[j + 1] != DOUBLE_MARK)
+		&& token->value[j + 1] != DOUBLE_MARK
+		&& token->value[j + 1] != SIMPLE_MARK)
 		j++;
 	token->value = expand_variable(token->value, *i, j, minishell);
 	(*i) = 0;
@@ -37,6 +38,12 @@ char	closed_quotes(char *ptr)
 		if (*ptr++ == DOUBLE_MARK)
 			return (1);
 	return (0);
+}
+
+char	get_global_marks(char *ptr)
+{
+	return (ptr[0] == DOUBLE_MARK
+		&& ptr[ft_strlen(ptr) - 1] == DOUBLE_MARK);
 }
 
 void	expand_tokens(t_token **tokens, t_minishell *minishell)
@@ -55,7 +62,8 @@ void	expand_tokens(t_token **tokens, t_minishell *minishell)
 			while (current->value[++i])
 			{
 				handle_marks(current->value[i], &marks, NULL, 0);
-				if (marks && current->value[i] == DOLLAR
+				if ((marks || get_global_marks(current->value))
+					&& current->value[i] == DOLLAR
 					&& closed_quotes(current->value + i))
 					if (!expand_loop(current, minishell, &i))
 						break ;
